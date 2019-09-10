4e1e325ff9f5f718ead8453df3c3580b8bb16c6d
heyimrlin <heyi_mrlin@163.com>
2019-09-10 17:40:57 +0800

    新增合同管理

---
+++ src/main/java/com/aoke/apartmentsystem/system/mapper/ContractMapper.java
@@ -0,0 +1,7 @@
+package com.aoke.apartmentsystem.system.mapper;
+
+import com.aoke.apartmentsystem.system.entity.Contract;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface ContractMapper extends BaseMapper<Contract> {
+}
